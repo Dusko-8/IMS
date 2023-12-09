b22d12590394ac9461fadf57fb10b8d1cb00b2db
Dušan Slúka <sluka.d8@gmail.com>
2023-12-09 19:54:34 +0100

    Documentation

--- visitor.cpp
+++ visitor.cpp
@@ -3,6 +3,7 @@
 
 Facility cashier1("Pokladna1");
 Facility cashier2("Pokladna2");
+Facility cashierAutomat2("PokladnaAutomat2");
 Facility cashierAutomat("PokladnaAutomat");
 
 Facility cafeteria("Cafeteria bar");
@@ -14,32 +15,56 @@ int Visitor::totalVisitors = 0;
 int Visitor::totalVisitors2 = 0;
 int Visitor::VolanteerLeave = 0;
 int Visitor::MidTicket = 0;
+int Visitor::SkipedCashier = 0;
+int Visitor::LeftInGap = 0;
+
+int LeftInPause = 0;
 
 class ExitEvent : public Event {
     Visitor* visitor;
 public:
     ExitEvent(Visitor* v) : visitor(v) {}
-
     void Behavior() override {
+        /*if(visitor->isInQueue()){
+            printf("In quue and leaving");
+        }*/
+        Visitor::totalVisitors2++;
         visitor->HandleLeave();
-        // If the visitor was dynamically allocated
     }
 
 };
 
 void Lift::Behavior() { 
         // Process up to 4 customers from the ski lift queue
+
         int processedCustomers = 0;
-        while (!skylift_queue.Empty() && processedCustomers < 4) {
-            Visitor *visitor = (Visitor *)skylift_queue.GetFirst(); // Get the first customer in the queue
-            visitor->Activate();
-            processedCustomers++;
-        }
-        if (Time >  10800 && Time < 10900){
-            Activate(Time + 7200);
+
+        /*if (Time >  21602 && Time <=21608){
+            printf("queue size - %d",skylift_queue.size());
+        }*/
+
+        if (Time >  14400 && Time <=21600){
+            printf("queue size - %d",skylift_queue.size());
+            for (size_t i = 0; i < skylift_queue.size(); i++)
+            {
+                Visitor *visitor = (Visitor *)skylift_queue.GetFirst(); // Get the first customer in the queue
+                visitor->Activate();
+            }
+
+            Activate(Time + 100);
+
         }
         else
         {
+            while (!skylift_queue.Empty() && processedCustomers < 4) {
+                Visitor *visitor = (Visitor *)skylift_queue.GetFirst(); // Get the first customer in the queue
+                //skylift_queue.operator delete(visitor);
+                //printf("queue size continuus - %d + %f\n",skylift_queue.size(),Time);
+                visitor->Activate();
+                processedCustomers++;
+            }
+            //printf("queue size - %d Time :%f \n",skylift_queue.size(),Time);
+            
             Activate(Time + 6);
         }
         
@@ -49,13 +74,12 @@ void Lift::Behavior() {
 Visitor::Visitor() {
     
     if (Random() <= chances.half_day_ticket_chance) {
-        /*if ((Time >  18000 && Time <=25200) || Time <=5400)
-        {*/
-            (new ExitEvent(this))->Activate(Time + 4 * 60 * 60);  // Assuming 60 time units = 1 hour
-            MidTicket++;
-        //}
+
+        (new ExitEvent(this))->Activate(Time + 4 * 60 * 60); 
+        MidTicket++;
+
     }
-    totalVisitors2++;
+    //totalVisitors2++;
     Activate();
 }
 
@@ -63,31 +87,42 @@ Visitor::Visitor() {
 
 // Main behavior of the customer
 void Visitor::Behavior() {
-    /*Mby add probability that he can leave*/ 
     HandlePayment();
 }
 
 void Visitor::HandlePayment() {
 
     if (!skipCashier) {
-        if (cashier1.QueueLen() <= cashier2.QueueLen() && cashier1.QueueLen() <= cashierAutomat.QueueLen())
+
+        if (cashier1.QueueLen() <= cashierAutomat2.QueueLen() && cashier1.QueueLen() <= cashierAutomat.QueueLen())
         {
             ProcessFacility(cashier1, static_data.cashier_selling_time);
 
-        }else if(cashier2.QueueLen() <= cashier1.QueueLen() && cashier2.QueueLen() <= cashierAutomat.QueueLen()){
-            ProcessFacility(cashier2, static_data.cashier_selling_time);
+        }else if(cashierAutomat2.QueueLen() <= cashier1.QueueLen() && cashierAutomat2.QueueLen() <= cashierAutomat.QueueLen()){
+            ProcessFacility(cashierAutomat2, static_data.automat_selling_time);
         }else{
             ProcessFacility(cashierAutomat, static_data.automat_selling_time);
         }
 
         totalVisitors += groupSize;
+        
         for (int i = 0; i < groupSize-1; ++i) {
             Visitor* groupMember = new Visitor();
             groupMember->skipCashier = true;
             groupMember->Activate();
         }
+
+    }else
+    {
+        SkipedCashier++;
     }
 
+    if (Time >  14400 && Time <=21600)
+    {
+        Wait(7201);
+        HandleSkying();
+    }
+    
     HandleSkying();
 }
 
@@ -97,7 +132,16 @@ void Visitor::HandleSkying() {
     // Check if the skylift is busy and enter the queue if necessary
     Into(skylift_queue);
     Passivate();
-    HandleSkiLift();
+
+    if (Time >  14400 && Time <=21600){
+        Visitor::LeftInGap++;
+        HandleLeave();
+        printf("Gong Home- %f \n",Time);
+    }
+    else{    
+        HandleSkiLift();
+        printf("Gong DownHill- %f \n",Time);
+    }
 }
 
 
@@ -112,19 +156,21 @@ void Visitor::HandleSkiLift() {
 void Visitor::HandleGoingDownHill() {
     
     Wait(RunData.customer_skying_Time);  //skiing down
-
     HandleActivity();
 }
 
 void Visitor::HandleActivity() {
     double next_step = Random();
-    if(next_step <= chances.exit_chance){
+
+    if ((Time >  14400 && Time <=21600) || Time > 28000){
+        Visitor::LeftInGap++;
+        HandleLeave();
+    }else if(next_step <= chances.exit_chance){
         VolanteerLeave++;
         HandleLeave();
     }
-    else if (next_step <= RunData.cafetria_visit_chance) {
+    else if (next_step >= RunData.cafetria_visit_chance) {
         HandleCafeteria();
-
     }else
     {
         HandleSkying();
@@ -165,27 +211,4 @@ void Visitor::ProcessFacility(Facility &f, double w) {
     f.Seize(this);
     Wait(Exponential(w));
     f.Release(this);
-}
-
-// Activate next customer in the queue
-void Visitor::ActivateQueue(Queue &q) {
-    if (!q.Empty()) {
-        Visitor *v = (Visitor *)q.GetFirst();
-        v->Activate();
-    }
-}
-
-// Activate next 4 customers in the queue
-int Visitor::Activate4Queue(Queue &q) {
-    int i = 0;
-    for(; i<4;i++){
-        if (!q.Empty()) {
-            Visitor *v = (Visitor *)q.GetFirst();
-            v->Activate();
-        }else
-        {
-            break;
-        }
-    }
-    return i;
 }
